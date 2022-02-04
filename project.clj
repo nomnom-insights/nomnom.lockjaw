0e9c335b52526d593fb8ab288b12db461e1ce509
AfonsoTsukamoto <afonsotsukamoto@gmail.com>
2022-02-04 18:55:40 +0900

    0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/lockjaw "0.3.0"
+(defproject nomnom/lockjaw "0.3.1-SNAPSHOT"
   :description "Postgres Advisory Locks as a Component"
   :url "https://github.com/nomnom-insights/nomnom.lockjaw"
   :license {:name "MIT License"
